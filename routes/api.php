ad092a0798974cee2a4cf4e39ae2b76bdd2640aa
abdumu <saanina@gmail.com>
2020-03-07 13:59:34 +0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
-
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/{method}', 'Api\ApiController')->middleware('frisk-api');
